d5fa420ea40063655eb86103851a34a52a3605f7
Rainbow-MRX <p.liashchynskyi@gmail.com>
2017-05-03 11:01:14 +0300

    add server script

--- off-robot.ino
+++ off-robot.ino
@@ -1,6 +1,8 @@
 #include <Roboton.h>
 #include <Ultrasonic.h>
 #include <Ticker.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 
 #define DISTANCE_RANGE 30
 #define SIZE_BUFF_D 3
@@ -13,57 +15,112 @@ MOTOR MOTOR_FRWB = {16, 4 ,5};
 MOTOR MOTOR_RGHL = {2, 14, 0};
 Roboton robot(&MOTOR_FRWB, &MOTOR_RGHL);
 
+bool offline = true;
 uint8_t distance[SIZE_BUFF_D];
 uint16_t prevMillis = 0;
 uint16_t currentMillis = 0;
+
+String webPage = "";
+
 Ticker tc_d;
 Ticker tc_c;
-Ticker tc_l;
-Ticker tc_r;
+Ticker tc_s;
+
+const char* ssid = "YOUR_SSID";
+const char* password = "YOUR_PASSWD";
+
+ESP8266WebServer server(80);
 
 void setup() {
-    tc_d.attach_ms(15, getDistance);
-    tc_c.attach_ms(27, checkCenter);
-    tc_r.attach_ms(34, checkRight);
-    tc_l.attach_ms(41, checkLeft);
+    tc_d.attach_ms(12, getDistance);
+    tc_c.attach_ms(17, checkCenter);
+    tc_s.attach_ms(22, checkSide);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print("NOT CONNECTED YET");
+    }
+    Serial.begin(115200);
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    server.on("/", [](){
+        server.send(200, "text/html", webPage);
+        offline = false;
+    });
+    server.on("/forward", [](){
+        server.send(200, "text/html", webPage);
+        robot.stop_mv(&MOTOR_FRWB);
+        robot.forward();
+    });
+    server.on("/back", [](){
+        server.send(200, "text/html", webPage);
+        robot.stop_mv(&MOTOR_FRWB);
+        robot.back();
+    });
+    server.on("/right", [](){
+        server.send(200, "text/html", webPage);
+        robot.right();
+    });
+    server.on("/left", [](){
+        server.send(200, "text/html", webPage);
+        robot.left();
+    });
+    server.begin();
+    Serial.println("HTTP server started");
+
 }
 void loop() {
-    
+    server.handleClient();
 }
 void getDistance() {
-    distance[0] = leftHC.Ranging(CM);
-    distance[1] = centerHC.Ranging(CM);
-    distance[2] = rightHC.Ranging(CM);
+    if (offline)
+    {
+        distance[0] = leftHC.Ranging(CM);
+        distance[1] = centerHC.Ranging(CM);
+        distance[2] = rightHC.Ranging(CM);
+    } else robot.stop_mv(&MOTOR_FRWB);
 }
-void checkLeft() {
-    if (distance[0] < DISTANCE_RANGE) {
-        robot.right();
-    } 
-    else {
-        robot.stop_mv(&MOTOR_RGHL);
-    }
+void checkSide() {
+    if (offline)
+    {
+        if (distance[0] < DISTANCE_RANGE && distance[2] > DISTANCE_RANGE) {
+            robot.right();
+        } 
+        else if (distance[2] < DISTANCE_RANGE && distance[0] > DISTANCE_RANGE) {
+            robot.left();
+        } 
+        else {
+            robot.stop_mv(&MOTOR_RGHL);
+        }
+    } else robot.stop_mv(&MOTOR_RGHL);
 }
 void checkCenter() {
-    if (distance[1] < DISTANCE_RANGE) {
+    if (offline)
+    {
+        if (distance[1] < DISTANCE_RANGE) {
         robot.stop_mv(&MOTOR_FRWB);
         robot.back();
         if ((currentMillis = millis()) - prevMillis > 200) {
             prevMillis = currentMillis;
             robot.stop_mv(&MOTOR_FRWB);
         }
-    } 
-    else {
-        robot.forward();
-    }
-    if (distance[0] < DISTANCE_RANGE && distance[2] < DISTANCE_RANGE) {
-        robot.stop_mv(&MOTOR_RGHL);
+        } 
+        else {
+            robot.forward();
+        }
     }
 }
-void checkRight() {
-    if (distance[2] < DISTANCE_RANGE) {
-        robot.left();
-    } 
-    else {
-        robot.stop_mv(&MOTOR_RGHL);
-    }
-}
+/**
+* var button = document.querySelector("button");
+  button.addEventListener("click", function(e){
+  e.preventDefault();
+  var xhr = new XMLHttpRequest()
+  xhr.open('GET', 'https://httpbin.org/get', true);
+  xhr.onload = function(e){
+    console.log( xhr.responseText );
+  };
+  xhr.send();  
+});
+**/
